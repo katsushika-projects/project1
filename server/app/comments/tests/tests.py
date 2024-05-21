f08afacd43607f76cd88021c4f58fb0aa4bee039
nakajimamoto340 <nakajimamoto340@gmail.com>
2024-05-21 09:50:31 +0900

    block機能テスト

--- server/app/comments/tests/tests.py
+++ server/app/comments/tests/tests.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-
 from items.models import Item
 from notifications.models import Notification
 
