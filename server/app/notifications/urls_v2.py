8fe98c738a7cb8e74fea9eb55fb14bfeab43299a
harune-pg <h.makino.22.6.533@4nonome.com>
2024-06-03 17:31:21 +0900

    namespaceをv2に対応,testをv1のみに適応

--- server/app/notifications/urls_v2.py
+++ server/app/notifications/urls_v2.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views_v2 import NotificationView, UpdateNotificationImportanceView
 
-# app_name = "notifications"
+app_name = "notifications_v2"
 
 urlpatterns = [
     path("", NotificationView.as_view(), name="notification-list"),
