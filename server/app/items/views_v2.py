86171c8978950b01fbcf66cd04a7ffb875aeebcb
harune-pg <h.makino.22.6.533@4nonome.com>
2024-06-12 08:02:57 +0900

    add commet-count

--- server/app/items/views_v2.py
+++ server/app/items/views_v2.py
@@ -10,6 +10,7 @@ from rest_framework.views import APIView
 
 from accounts.models import Block
 from notifications.models import Notification
+from comments.models import Comment
 
 from .models import Item, Like, Report
 from .serializers import ItemCreateSerializer, ItemReportSerializer, ItemSerializer
@@ -110,9 +111,12 @@ class ItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         exclude_user_id_list = Block.create_exclude_user_id_list_by_request_user(request.user)
         if item.seller.id in exclude_user_id_list:
             return Response({"detail": "ブロック中、被ブロック中のユーザーの商品は閲覧できません。"}, status=status.HTTP_400_BAD_REQUEST)
+        comment_count = Comment.objects.filter(item_id=item.id).exclude(user__in=exclude_user_id_list).count()
+        
+        response_data = self.get_serializer(item).data
+        response_data["comment_count"] = comment_count
 
-        serializer = self.get_serializer(item)
-        return Response(serializer.data)
+        return Response(response_data, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
         partial = self.request.query_params.get("partial", False).lower() in ["true", "1", "t"]
